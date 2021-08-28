d3ab55736537d60d6636ad147e518a941e9c5241
Prabh <prabhoz@yahoo.com.au>
2021-08-28 22:24:30 +0930

    added login card as per upgrad

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -6,6 +7,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import './Header.css';
 //Header component added
 class Header extends Component {
@@ -14,7 +16,10 @@ class Header extends Component {
         //added state
         this.state = {
             username:"",
-            password:""
+            password:"",
+            reqUsername: "dispNone",
+            reqPassword: "dispNone",
+            error: "dispNone",
         }
     }
         //username change handler
@@ -25,6 +30,22 @@ class Header extends Component {
     inputPasswordChangeHandler = (e) => {
         this.setState({ password: e.target.value});
     }
+    //login button handler added
+    loginButtonHandler = () => {
+        this.state.username === "" ? this.setState({reqUsername: "dispBlock"}): this.setState({reqUsername:"dispNone"});
+        this.state.password === "" ? this.setState({reqPassword: "dispBlock"}):this.setState({reqPassword:"dispNone"});
+        let correctUsername = "Prabh";
+        let correctPassword ="Password";
+        if(this.state.username === correctUsername && this.state.password === correctPassword){
+            ReactDOM.render(<div>Home Page</div>, document.getElementById('root'));
+        }
+        else{
+            if(this.state.username !== "" && this.state.password !== "")
+            this.setState({error: "dispBlock"});
+        }
+
+
+    }
     render(){
         return(
             <div>
@@ -33,27 +54,32 @@ class Header extends Component {
                       Image Viewer
                     </p>
                 </header><br/><br/>
-                
-                
-                
-                <Card className="cardStyle mdc-card">
+                <Card className="cardStyle">
                     <CardContent>
-                        <Typography variant="headline" component="h1">
+                        <Typography variant="h4">
                                LOGIN
-                        </Typography> <br/><br/>
+                        </Typography> <br/>
                         <FormControl required className="formControl"> 
                             <InputLabel htmlFor="username">Username</InputLabel>
                             <Input id="username" type="text" username={this.state.username}
                             onChange={this.inputUsernameChangeHandler}/>
+                        <FormHelperText className={this.state.reqUsername}><span className="red">required</span></FormHelperText>
                          </FormControl><br/><br/>
                         <FormControl required className="formControl">
                             <InputLabel htmlFor="password">Password</InputLabel>
                              <Input id="password" type="password" password={this.state.password}
                              onChange={this.inputPasswordChangeHandler}/>
-                         </FormControl> <br/><br/><br/>
-                            <Button variant="contained" color="primary">Login</Button>
+                        <FormHelperText className={this.state.reqPassword}><span className="red">required</span></FormHelperText>
+                        </FormControl> <br/><br/>
+                         <FormControl required className="formControl">
+                         <FormHelperText className={this.state.error}><span className="red">Incorrect username and/or password</span></FormHelperText>
+                         </FormControl><br/><br/>
+                            <Button variant="contained" onClick={this.loginButtonHandler} color="primary">
+                                Login
+                            </Button>
                     </CardContent>
                 </Card>
+
                
                 
             </div>
